97540fc3196970c4cfc5ff770c6d58489e9f42ab
arijitkar <demonicak07@gmail.com>
2025-12-30 12:56:22 +0000

    first commit

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# S3_BUCKET=s3://image-captioning-models
+# MODEL_NAME=caption_model_v1.pth
+# DEST_PATH=inference/models/model.pth
+
+# mkdir -p inference/artifacts
+
+echo "Downloading model from S3..."
+# aws s3 cp $S3_BUCKET/$MODEL_NAME $DEST_PATH
+
+echo "Model ready for inference."
+
+echo "Running startup tasks..."
+
+echo "Starting FastAPI..."
+exec uvicorn inference.app:app --host 0.0.0.0 --port 8000
